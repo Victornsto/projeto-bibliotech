2a448e1296d8c4b23de6ff6009cfc9216696b728
Victor Augusto Cunha Nascimento <victornsto@gmail.com>
2022-11-21 22:33:14 -0300

    Correção no script sql

--- src/resources/sqlbibliotech.sql
+++ src/resources/sqlbibliotech.sql
@@ -11,24 +11,21 @@ CREATE DATABASE bibliotech
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
-CREATE TABLE IF NOT EXISTS public.usuarios
-(
-    useid integer NOT NULL DEFAULT nextval('usuarios_useid_seq'::regclass),
-    nome character varying COLLATE pg_catalog."default",
-    email character varying COLLATE pg_catalog."default",
-    senha character varying COLLATE pg_catalog."default",
-    adm boolean DEFAULT false,
-    CONSTRAINT usuarios_pkey PRIMARY KEY (useid),
-    CONSTRAINT usuarios_email_key UNIQUE (email)
+    
+CREATE TABLE IF NOT EXISTS public.usuarios (
+    useid serial primary key,
+    nome varchar not null, 
+    email varchar unique not null,
+    senha varchar not null,
+    adm boolean DEFAULT false
 );
 
 insert into usuarios (Nome, Email, Senha, Adm) values ('Administrador', 'Adm@bibliotech.com', 'adm@2022', true);
 CREATE TABLE IF NOT EXISTS public.livros
 (
-    livroid integer NOT NULL DEFAULT nextval('livros_livroid_seq'::regclass),
-    nome_livro character varying(50) COLLATE pg_catalog."default",
-    nome_autor character varying(50) COLLATE pg_catalog."default",
+    livroid serial NOT NULL,
+    nome_livro varchar not null,
+    nome_autor varchar not null,
     data_cad date NOT NULL DEFAULT CURRENT_DATE,
-    useid numeric,
-    CONSTRAINT livros_pkey PRIMARY KEY (livroid)
+    useid numeric not null
 );
